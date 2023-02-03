2fd758e7dfb4403ff7aa94ca826d0461f4405057
riyazur razak <riyazurrazaknoushath786@gmail.com>
2023-02-03 10:41:17 +0530

    password test updated

--- cli/tests/password.test.js
+++ cli/tests/password.test.js
@@ -1,9 +1,18 @@
 import { generatePassword } from "../src/service/password";
 
-describe("Generate Unique Password", () => {
+describe("Generate Unique Password with String Password Ruleset", () => {
+  const regex = /^(?=.*[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
   for (let i = 1; i <= 100; i++) {
+    const passwordA = generatePassword();
+    const passwordB = generatePassword();
+    test(`${i} A password = ${passwordA}`, () => {
+      expect(regex.test(passwordA)).toBe(true);
+    });
+    test(`${i} B password = ${passwordB}`, () => {
+      expect(regex.test(passwordB)).toBe(true);
+    });
     test(`${i} time`, () => {
-      expect(generatePassword()).not.toBe(generatePassword());
+      expect(passwordA).not.toBe(passwordB);
     });
   }
 });
